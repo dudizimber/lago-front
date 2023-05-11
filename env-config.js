bca55903ce4be030895c0e33e65d2ac7f97cd66e
Dudi Zimberknopf <dudi@vizo.dev>
2023-05-11 12:52:44 +0300

    deploy

---
+++ env-config.js
@@ -0,0 +1,5 @@
+window.API_URL = "https://api.lago.vizo.dev"
+window.APP_ENV = "production"
+window.LAGO_OAUTH_PROXY_URL = "https://proxy.lago.vizo.dev"
+window.LAGO_DISABLE_SIGNUP = "true"
+window.SENTRY_DSN = ""
